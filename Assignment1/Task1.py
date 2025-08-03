9b2dc215aef382f57145daa901db4717422a6866
Tanishq <tanishq02022005@gmail.com>
2025-08-03 16:48:25 +0530

    Initial commit

---
+++ Assignment1/Task1.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter your first number:"))
+num2=int(input("Enter your second number:"))
+
+print("Addition:",num1+num2)
+print("Subtraction:",num1-num2)
+print("Multiplication:",num1*num2)
+print("Division:",num1/num2 if(num2!=0) else "Division by zero not defined")
+
---
+++ Assignment1/Task2.py
@@ -0,0 +1,4 @@
+firstName = input("Enter first name: ")
+lastName = input("Enter last name: ")
+
+print(f"Hello {firstName} {lastName}! Welcome to the Python program.")
